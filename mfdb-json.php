4fd487bd9785c0f9333712206ba48c89cd284020
Robert Kulagowski <rkulagow@schedulesdirect.org>
2013-06-24 13:23:19 -0500

    Start work on grabber.

--- mfdb-json.php
+++ mfdb-json.php
@@ -1,4 +1,44 @@
+<?php
 /*
-* PHP grabber for Schedules Direct JSON data.
-*/
+ * This file is a grabber which downloads data from Schedules Direct's JSON service.
+ */
 
+$isBeta = TRUE;
+date_default_timezone_set("America/Chicago");
+$date = new DateTime();
+$todayDate = $date->format("Y-m-d");
+
+$user = "mythtv";
+$password = "mythtv";
+$host = "192.168.10.10";
+$db = "mythconverg";
+
+$longoptions = array("beta::","host::","user::","password::");
+
+$options = getopt("", $longoptions);
+foreach ($options as $k => $v)
+{
+    switch ($k)
+    {
+        case "beta":
+            $isBeta == TRUE;
+            break;
+    }
+}
+
+print "Attempting to connect to database.\n";
+
+try
+{
+    $dbh = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $password,
+        array(PDO::ATTR_PERSISTENT => true));
+    $dbh->exec("SET CHARACTER SET utf8");
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING);
+} catch (PDOException $e)
+{
+    print "Exception with PDO: " . $e->getMessage() . "\n";
+    exit;
+}
+
+
+?>
