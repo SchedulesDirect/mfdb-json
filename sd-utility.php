5d65b8852d5edea1b6d50359f9423b5ee07d27df
Robert Kulagowski <rkulagow@schedulesdirect.org>
2014-02-16 02:31:42 -0600

    WIP.

--- sd-utility.php
+++ sd-utility.php
@@ -662,69 +662,6 @@ function getStatus()
 }
 */
 
-function printStatus()
-{
-    global $dbh;
-    global $sdStatus;
-    global $updatedHeadendsToRefresh;
-
-    print "\nStatus messages from Schedules Direct:\n";
-
-    if ($sdStatus["code"] == 0)
-    {
-        $expires = $sdStatus["account"]["expires"];
-        $maxHeadends = $sdStatus["account"]["maxLineups"];
-        $nextConnectTime = $sdStatus["account"]["nextSuggestedConnectTime"];
-
-        foreach ($sdStatus["account"]["messages"] as $a)
-        {
-            print "MessageID: " . $a["msgID"] . " : " . $a["date"] . " : " . $a["message"] . "\n";
-        }
-    }
-    else
-    {
-        print "Received error response from server!\n";
-        print "ServerID: " . $sdStatus["serverID"] . "\n";
-        print "Message: " . $sdStatus["message"] . "\n";
-        print "\nFATAL ERROR. Terminating execution.\n";
-        exit;
-    }
-
-    print "Server: " . $sdStatus["serverID"] . "\n";
-    print "Last data refresh: " . $sdStatus["lastDataUpdate"] . "\n";
-    print "Account expires: $expires\n";
-    print "Max number of headends for your account: $maxHeadends\n";
-    print "Next suggested connect time: $nextConnectTime\n";
-
-    $getLocalModified = $dbh->prepare("SELECT modified FROM SDheadendCache WHERE lineup=:he");
-    print "The following lineups are in your account at Schedules Direct:\n\n";
-
-    $he = getSchedulesDirectHeadends();
-
-    if (count($he))
-    {
-        foreach ($he as $id => $modified)
-        {
-            $line = "$id\t Last Updated: $modified";
-
-            $getLocalModified->execute(array("he" => $id));
-            $sdStatusult = $getLocalModified->fetchAll(PDO::FETCH_COLUMN);
-
-            if ((count($sdStatusult) == 0) OR ($sdStatusult[0] < $modified))
-            {
-                $updatedHeadendsToRefresh[$id] = $modified;
-                $line .= " (*** Update Available ***)";
-            }
-
-            print "$line\n";
-        }
-
-        if (count($updatedHeadendsToRefresh))
-        {
-            updateLocalHeadendCache($updatedHeadendsToRefresh);
-        }
-    }
-}
 
 function updateLocalHeadendCache(array $updatedHeadendsToRefresh)
 {
