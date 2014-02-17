8d1390aba3cf5083d43cdc15db29b99d96bf4af2
Robert Kulagowski <rkulagow@schedulesdirect.org>
2014-02-17 14:15:41 -0600

    Cast channel as string for ota analog.

--- sd-utility.php
+++ sd-utility.php
@@ -518,7 +518,7 @@ function printLineup()
         var_dump($stationMap[$stationID]);
         var_dump($stationID);
 
-        $stationData->appendRow(array($channel, $stationMap[$stationID], (string)$stationID));
+        $stationData->appendRow(array((string)$channel, $stationMap[$stationID], (string)$stationID));
     }
 
     print $stationData;
