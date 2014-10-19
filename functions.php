49b0450c3f2f9b57698d81dd008f7fcc7d3c440b
Robert Kulagowski <rkulagow@schedulesdirect.org>
2014-10-19 17:46:56 -0500

    Set as known broken for now.

--- functions.php
+++ functions.php
@@ -20,7 +20,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 $scriptVersion = "0.19-sqlite";
 $scriptDate = "2014-10-18";
-$knownToBeBroken = FALSE;
+$knownToBeBroken = TRUE;
 
 function getToken($username, $passwordHash)
 {
