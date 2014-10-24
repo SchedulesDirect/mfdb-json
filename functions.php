15dc9c89dda046117fdda0016fab69bb818baff5
Robert Kulagowski <rkulagowski@mythtv.org>
2014-10-24 08:54:19 -0500

    Bump the version number to reflect the new URL.

--- functions.php
+++ functions.php
@@ -18,8 +18,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-$scriptVersion = "0.19-sqlite";
-$scriptDate = "2014-10-20";
+$scriptVersion = "0.20-sqlite";
+$scriptDate = "2014-10-24";
 $knownToBeBroken = FALSE;
 
 function getToken($username, $passwordHash)
