6d139f6ef5ab73374d163060cbdd63bf34cb8c9f
Robert Kulagowski <rkulagowski@mythtv.org>
2014-10-27 09:55:37 -0500

    Bump date.

--- functions.php
+++ functions.php
@@ -19,7 +19,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
 $scriptVersion = "0.20-sqlite";
-$scriptDate = "2014-10-24";
+$scriptDate = "2014-10-27";
 $knownToBeBroken = FALSE;
 
 function getToken($username, $passwordHash)
