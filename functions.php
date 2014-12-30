c577e0afa3a0ddad8d85b07cbf08ccf1682ab96e
Robert Kulagowski <rkulagowski@mythtv.org>
2014-12-30 15:54:15 -0600

    Since this is on a dedicated branch, leave the version the same as production so that we don't need to provide the override.

--- functions.php
+++ functions.php
@@ -18,8 +18,8 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-$scriptVersion = "0.26";
-$scriptDate = "2014-12-12";
+$scriptVersion = "0.25";
+$scriptDate = "2014-12-30";
 $knownToBeBroken = FALSE;
 $skipVersionCheck = TRUE;
 
