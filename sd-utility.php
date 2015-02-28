ac202744a7728d22d1ecca5398ad51d2f1288b35
Robert Kulagowski <rkulagow@schedulesdirect.org>
2015-02-28 15:13:25 -0600

    Set isBeta to true so that we start testing the 20141201 API.

--- sd-utility.php
+++ sd-utility.php
@@ -21,7 +21,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-$isBeta = FALSE;
+$isBeta = TRUE;
 $debug = FALSE;
 $done = FALSE;
 $isMythTV = TRUE;
