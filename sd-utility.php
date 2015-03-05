daece0784a43accc28764163b78723cfa027f127
Robert Kulagowski <rkulagowski@mythtv.org>
2015-03-05 12:35:10 -0600

    Determine which countries have just one postal code instead of maintaining separate arrays.

--- sd-utility.php
+++ sd-utility.php
@@ -1354,76 +1354,60 @@ function printLineup()
 
 function addLineupsToSchedulesDirect()
 {
-global $client;
-global $debug;
-global $token;
-global $availableCountries;
+    global $client;
+    global $debug;
+    global $token;
+    global $availableCountries;
 
-$sdLineupArray = array();
+    $sdLineupArray = array();
+    $countriesWithOnePostalCode = array();
 
-$arrayCountriesWithOnePostalCode = array(
-    "BLZ" => "BZ",
-    "COL" => "CO",
-    "GUY" => "GY",
-    "HND" => "HN",
-    "PAN" => "PA",
-    "AIA" => "AI-2640",
-    "ATG" => "AG",
-    "ABW" => "AW",
-    "BHS" => "BS",
-    "BES" => "BQ",
-    "CUW" => "CW",
-    "DMA" => "DM",
-    "GRD" => "GD",
-    "MAF" => "97150",
-    "KNA" => "KN",
-    "LCA" => "LC",
-    "TTO" => "TT",
-    "TCA" => "TKCA1ZZ");
-
-$done = FALSE;
-$country = "";
-
-while ($done === FALSE)
-{
-    print "Three-character ISO-3166-1 alpha3 country code (? to list available countries):";
-    $country = strtoupper(readline(">"));
-
-    if ($country == "")
+    foreach ($availableCountries as $region => $foo)
     {
-        return;
-    }
-
-    if ($country == "?")
-    {
-        printListOfAvailableCountries(TRUE);
-    }
-    else
-    {
-        $done = TRUE;
-    }
-}
-
-foreach ($availableCountries as $region => $foo)
-{
-    foreach ($foo as $item)
-    {
-        if (isset($item["shortName"]["onePostalCode"]) === TRUE)
+        foreach ($foo as $item)
         {
-            print "Only one valid postal code for this country: {$item["shortName"]["onePostalCode"]}\n";
-            $postalcode = $item["shortName"]["onePostalCode"];
-            break;
+            if (isset($item["shortName"]["onePostalCode"]) === TRUE)
+            {
+                $countriesWithOnePostalCode[$item["shortName"]] = $item["shortName"]["postalCodeExample"];
+            }
+        }
+    }
+
+    $done = FALSE;
+    $country = "";
+
+    while ($done === FALSE)
+    {
+        print "Three-character ISO-3166-1 alpha3 country code (? to list available countries):";
+        $country = strtoupper(readline(">"));
+
+        if ($country == "")
+        {
+            return;
+        }
+
+        if ($country == "?")
+        {
+            printListOfAvailableCountries(TRUE);
         }
         else
         {
-            print "Enter postal code:";
-            $postalcode = strtoupper(readline(">"));
+            $done = TRUE;
+        }
+    }
 
-            if ($postalcode == "")
-            {
-                return;
-            }
-            break;
+    if (isset($countriesWithOnePostalCode[$country]) === TRUE)
+    {
+        $postalCode = $countriesWithOnePostalCode[$country];
+    }
+    else
+    {
+        print "Enter postal code:";
+        $postalcode = strtoupper(readline(">"));
+
+        if ($postalcode == "")
+        {
+            return;
         }
     }
 
@@ -1523,6 +1507,7 @@ foreach ($availableCountries as $region => $foo)
     }
 
     print "Message from server: {$res["message"]}\n";
+}
 }
 
 function directAddLineup($lineup)
