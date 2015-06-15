de8879aba6127499e0680ac5305f679fcd494bea
Robert Kulagowski <rkulagowski@mythtv.org>
2015-06-15 15:03:48 -0500

    Now that the incremental data update bug is fixed, let users know that they should get the new client.

--- functions.php
+++ functions.php
@@ -18,10 +18,10 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-$scriptVersion = "0.25";
-$scriptDate = "2015-05-18";
+$scriptVersion = "0.26";
+$scriptDate = "2015-06-15";
 $knownToBeBroken = FALSE;
-$skipVersionCheck = TRUE;
+$skipVersionCheck = FALSE;
 
 $tz = "UTC";
 date_default_timezone_set($tz);
