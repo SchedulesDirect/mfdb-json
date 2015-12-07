f06540b838879f64b1b719cbfadb417d0522eb56
rkulagowski <rkulagowski@mythtv.org>
2015-12-06 21:17:59 -0600

    Create a function that links a capture card to a video source.

--- sd-utility.php
+++ sd-utility.php
@@ -492,11 +492,17 @@ while ($done === false) {
 
     if ($isMythTV === true) {
         print "\nMythTV functions:\n";
+        print "Videosource functions\n-----------------\n";
         print "A to Add a new videosource to MythTV\n";
         print "D to Delete a videosource in MythTV\n";
-        print "E to Extract Antenna / QAM / DVB scan from MythTV to send to Schedules Direct\n";
         print "L to Link a videosource to a lineup at Schedules Direct\n";
-        print "U to update a videosource by downloading from Schedules Direct\n";
+        print "U to Update a videosource by downloading from Schedules Direct\n";
+
+        print "Capture card functions\n---------------------\n";
+        print "C to Connect a capture card input to a videosource\n";
+
+        print "Miscellaneous\n-----------------\n";
+        print "E to Extract Antenna / QAM / DVB scan from MythTV to send to Schedules Direct\n";
     }
 
     print "Q to Quit\n";
