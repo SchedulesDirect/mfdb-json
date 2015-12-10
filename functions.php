0e40b523931693fb054c2b1c6255557203fe55d7
Robert Kulagowski <rkulagowski@mythtv.org>
2015-12-10 14:28:23 -0600

    Update code to troubleshoot why user is running into schedulesdirect.settings not existing.

--- functions.php
+++ functions.php
@@ -18,10 +18,10 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-$scriptVersion = "0.28";
-$scriptDate = "2015-12-04";
+$scriptVersion = "0.28-dev";
+$scriptDate = "2015-12-10";
 $knownToBeBroken = false;
-$skipVersionCheck = false;
+$skipVersionCheck = true;
 
 $dbName = null;
 $dbUser = null;
